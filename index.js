a678695bc5163330127dbf26b691256885052206
rupesh-saha <saharupesh291@gmail.com>
2026-05-28 13:57:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.LARAVA_URI;
+const app = express();
+const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('laranaDB');
+    const doctorsCollection = db.collection('doctors');
+    const featuredDoctors = db.collection('featured-doctors');
+    const appointmentsCollection = db.collection('appointments');
+
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/featured-doctors", async (req, res) => {
+      const result = await featuredDoctors.find().toArray();
+      res.send(result);
+    });
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running for Larana!");
+});
+
